25b928dbf3c5c6549c8df225732ea2ff63db9da9
sonu <alliansonu@gmail.com>
2021-10-18 03:14:35 +0530

    10oct

--- JS/index.js
+++ JS/index.js
@@ -7,13 +7,63 @@ document.querySelector(".crossClose").addEventListener("click", () =>{
 })
 
 document.querySelector("#get-started-btn").addEventListener("click", () =>{
-  
+
   document.querySelector(".getStartedCoverLeftShodow").style.display = "block";
   document.querySelector(".getStartedCoverTopShodow").style.display = "block";
   document.querySelector(".getStartedCoverButtomShodow").style.display = "block";
   document.querySelector(".getStartedCoverRightShodow").style.display = "flex";
 })
 
+document.querySelector(".aboutSchoolInPhoneLoginSignUp").addEventListener("click", () =>{
+
+  document.querySelector(".home-page").style.left = "-100vw";
+  document.querySelector(".home-page").style.transitionDuration = "1s";
+  document.querySelector(".home-page-video").style.left = "-95vw";
+  document.querySelector(".home-page-video").style.transitionDuration = "1s";
+  //document.querySelector(".loginSignUp").style.zIndex = "50";
+
+  setTimeout(function(){
+    document.querySelector(".loginSignUp").style.zIndex = "50";
+  }, 1000)
+
+  for (let i = 0; i <= menus.length; i++) {
+    makeDisplayNoneForMenuPages(menus[i]);
+  }
+  function makeDisplayNoneForMenuPages(menuName){
+    document.querySelector("." + menuName).style.display="none";
+  }
+
+  // document.querySelector(".getStartedCoverTopShodow").style.display = "block";
+  // document.querySelector(".getStartedCoverButtomShodow").style.display = "block";
+  // document.querySelector(".getStartedCoverRightShodow").style.display = "flex";
+})
+
+
+document.querySelector(".backToHompageFromLoginSignUpArrow").addEventListener("click", () =>{
+
+  document.querySelector(".home-page").style.left = "0vw";
+  document.querySelector(".home-page").style.transitionDuration = "0.5s";
+  document.querySelector(".home-page-video").style.left = "5vw";
+  document.querySelector(".home-page-video").style.transitionDuration = "0.5s";
+  document.querySelector(".loginSignUp").style.zIndex = "-10";
+
+
+  setTimeout(function() {
+  // code to be executed after 0.5 second
+  for (let i = 0; i <= menus.length; i++) {
+    makeDisplayBlockForMenuPages(menus[i]);
+  }
+  function makeDisplayBlockForMenuPages(menuName){
+    document.querySelector("." + menuName).style.display="block";
+  }
+  }, 500);
+
+  
+
+  // document.querySelector(".getStartedCoverTopShodow").style.display = "block";
+  // document.querySelector(".getStartedCoverButtomShodow").style.display = "block";
+  // document.querySelector(".getStartedCoverRightShodow").style.display = "flex";
+})
 
 function showingMenu(item) {
   document.getElementById(item).addEventListener("click", () => {
@@ -48,4 +98,3 @@ for (let i = 0; i <= menus.length; i++) {
   showingMenu(menus[i]);
   closingMenu(menuClose[i], menus[i]);
 }
-
